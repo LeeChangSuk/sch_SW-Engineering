ca7bf0ab60cb65b3269b48b5609b9c5bf10c62c8
DESKTOP-ULPCTH7\82107 <changseog8701@gmail.com>
2022-09-24 01:18:53 +0900

    test.py
    깃허브 사용방법을 익히기 위한 파일

---
+++ test.py
@@ -0,0 +1,26 @@
+# 간단한 계산기 예제
+class Calculator:
+
+    def init():
+        result = 0
+    # 더하기
+    def adder(x, y):
+        result = x + y
+        return result
+    
+    def adder(x, y, z):
+        result = x + y + z
+        return result
+    # 빼기
+    def substract(x, y):
+        result = x - y
+        return result
+    def substract(x, y, z):
+        result = x - y - z
+        return result
+
+Calculator.init()
+x = 10
+y = 20
+z = 50
+print(Calculator.adder(x, y, z))
